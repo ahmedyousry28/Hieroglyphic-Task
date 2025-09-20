691d7e1adb9e4720b8a09415bec67a8f3fd7c101
ahmedyousry28 <ahmeddidamony9@gmail.com>
2025-09-20 06:02:28 +0300

    add border

--- src/components/bar/DirectionIcons.tsx
+++ src/components/bar/DirectionIcons.tsx
@@ -1,22 +1,29 @@
 import { TextAlignEnd, TextAlignStart } from "lucide-react";
-import { useAppDispatch } from "../../store/store";
+import { useAppDispatch, useAppSelector } from "../../store/store";
 import { setOptions } from "../../store/optionsSlice";
 const DirectionIcons = () => {
+  const { direction } = useAppSelector((state) => state.options);
   const dispatch = useAppDispatch();
   return (
     <div className="flex text-2xl gap-2">
+      <TextAlignStart
+        onClick={() => dispatch(setOptions({ direction: "ltr" }))}
+        className={`${
+          direction === "ltr" ? "border border-[#666666]" : "border-none"
+        }`}
+      >
+        <title>LTR (Left to Right)</title>
+      </TextAlignStart>
       <TextAlignEnd
         onClick={() => {
           dispatch(setOptions({ direction: "rtl" }));
         }}
+        className={`${
+          direction === "rtl" ? "border border-[#666666]" : "border-none"
+        }`}
       >
         <title>RTL (Right to Left)</title>
       </TextAlignEnd>
-      <TextAlignStart
-        onClick={() => dispatch(setOptions({ direction: "ltr" }))}
-      >
-        <title>LTR (Left to Right)</title>
-      </TextAlignStart>
     </div>
   );
 };
